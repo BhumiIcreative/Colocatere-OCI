521765fb0ea83dd520d7d109d77d94a2f087a64b
khushi <khushi.dadhaniya@icreativetechnolabs.com>
2024-10-15 15:21:31 +0530

    [ADD][account_payment_group_by_project]Added migrated module

---
+++ account_payment_group_by_project/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Account Payment group by project",
+    "summary": """
+        Add group by project to payment register""",
+    "version": "1.0",
+    "license": "AGPL-3",
+    "author": "Aktiv software",
+    "depends": ["account"],
+    "data": [
+        "views/account_payment_view.xml",
+    ],
+}
